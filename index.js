f31d189761fc90055e14579dae98397175e1c5a8
Anton Kharitonov <anton.kharitonov@sap.com>
2024-09-26 10:15:54 +0200

    Add basic HTTP server implementation in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const {createServer} = require('node:http');
+
+const server = createServer((req, res) => {
+    res.statusCode(200);
+
+    res.end("Hi");
+})
+
+server.listen(3000, '127.0.0.1', ()=>{
+    //log out that the server is running on port 3000
+    console.log('Server is running on port 3000');
+    
+})
